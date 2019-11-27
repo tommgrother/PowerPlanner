23e7233da463d3c8edc990f6406346626c2740de
ryucxd <ryucxd@gmail.com>
2019-11-27 15:59:28 +0000

    needs to fire sql

--- ShopFloorPlacementPlanner/frmWeeklyOT.Designer.cs
+++ ShopFloorPlacementPlanner/frmWeeklyOT.Designer.cs
@@ -29,24 +29,50 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(114, 61);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(500, 235);
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(272, 255);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(12, 273);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_cancel.TabIndex = 1;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(209, 273);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(75, 23);
+            this.btn_update.TabIndex = 2;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
             // 
             // frmWeeklyOT
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(296, 328);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_cancel);
             this.Controls.Add(this.dataGridView1);
             this.Name = "frmWeeklyOT";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmWeeklyOT";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
@@ -56,5 +82,7 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.Button btn_update;
     }
 }
--- ShopFloorPlacementPlanner/frmWeeklyOT.cs
+++ ShopFloorPlacementPlanner/frmWeeklyOT.cs
@@ -44,7 +44,7 @@ namespace ShopFloorPlacementPlanner
             //dataGridView1.Columns[1].HeaderText = "Department Over Time";
             using (SqlConnection CONNECT = new SqlConnection(connectionStrings.ConnectionString))
             {
-                string sql = "Select CAST(a.date_plan as date), b." + dept + "_OT" +
+                string sql = "Select a.id,CAST(a.date_plan as date), b." + dept + "_OT" +
                      " FROM dbo.power_plan_date a " +
                      "LEFT JOIN dbo.power_plan_overtime b on b.date_id = a.id " +
                      "WHERE date_plan >= '" + Monday.ToString("yyyyMMdd") + "' AND date_plan <= '" + Sunday.ToString("yyyyMMdd") + "' ORDER BY date_plan ASC";
@@ -58,11 +58,40 @@ namespace ShopFloorPlacementPlanner
                     CONNECT.Close();
                 }
             }
+            this.dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[0].HeaderText = "ID";
+            dataGridView1.Columns[1].HeaderText = "Date";
+            dataGridView1.Columns[2].HeaderText = dept + " Over Time";
+            dataGridView1.Columns[0].ReadOnly = true;
+            dataGridView1.Columns[1].ReadOnly = true;
+        }
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            //update each line here to add new overtime, even if it hasnt been changed then it should not matter because it pulls the current overtime into that cell
+            using (SqlConnection CONNECT = new SqlConnection(connectionStrings.ConnectionString))
+            {
+                string sql = "";
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    sql = "update dbo.power_plan_overtime SET " + dept + "_OT  = " + dataGridView1.Rows[i].Cells[2].Value.ToString() + " WHERE date_id = " + dataGridView1.Rows[i].Cells[0].Value.ToString() + "";
+                    using (SqlCommand COMMAND = new SqlCommand(sql, CONNECT))
+                    {
+                        MessageBox.Show(sql); // ready to fire!!!!!
+                    }
+                }
+            }
         }
 
         //public static DateTime FirstDateInWeek(this DateTime dt)
         //{
 
         //    return dt;
-        }
+    }
    }
